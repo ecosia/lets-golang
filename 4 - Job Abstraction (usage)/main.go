296e875fc690cce601cf053853556afc9fa7874c
Luuk Verweij <luuk.verweij@gig.com>
2019-10-15 12:29:29 +0200

    fixes chapter 4

--- 4 - Job Abstraction (usage)/main.go
+++ 4 - Job Abstraction (usage)/main.go
@@ -8,18 +8,21 @@ import (
 )
 
 func waitForResponse(results chan jobs.JobResult) {
+
 	result := <-results // PYTHON: You could build something similar with queue and threads
+
 	if result.Err != nil {
 		fmt.Printf("%s failed: %s\n", result.ID, result.Err.Error())
-	} else {
-		switch res := result.Result.(type) { // PYTHON: similar to isinstance
-		case *api.HexbotResponse: // PYTHON: if/elif/else
-			fmt.Printf("Hexbot successful: %+v\n", res)
-		case *api.VexbotResponse:
-			fmt.Printf("Vexbot successful: %+v\n", res)
-		default:
-			fmt.Printf("Unknown successful: %+v\n", res)
-		}
+                return
+	}
+
+	switch res := result.Result.(type) { // PYTHON: similar to isinstance
+	case *api.HexbotResponse: // PYTHON: if/elif/else
+		fmt.Printf("Hexbot successful: %+v\n", res)
+	case *api.VexbotResponse:
+		fmt.Printf("Vexbot successful: %+v\n", res)
+	default:
+		fmt.Printf("Unknown successful: %+v\n", res)
 	}
 }
 
